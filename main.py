81471584051af92c28d87c6a97c52acb68ffad41
Xhan <alvaradotuctod@gmail.com>
2022-11-26 00:55:55 -0500

    for, blocks

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
+
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar producto']
 
 
 @app.route('/')
 def index():
     """
-    get the user's ip, and saved ip user in a cookie
-    and finally we are going to redirect it to the /hello route
+    * get the user's ip, and saved ip user in a cookie
+    * finally we are going to redirect it to the /hello route
     """
 
     user_ip = request.remote_addr
@@ -22,10 +25,17 @@ def index():
 @app.route('/hello')
 def hello():
     """
-    get cookie user
+    * get cookie user
+    * render html and we passed the user ip
     """
     user_ip = request.cookies.get('user_ip')
-    return f'Hola te hackee, tu ip es: {user_ip}'
+
+    kwargs = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    
+    return render_template('hello.html', **kwargs)
 
 
 
